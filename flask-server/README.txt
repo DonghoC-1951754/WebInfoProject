fb8c6b8b5d0d6821d6475a94e0d6cfa6a998aa6f
SeppeK-1952233 <seppe.kimps@student.uhasselt.be>
2024-11-25 20:41:16 +0100

    start graphQL

---
+++ flask-server/README.txt
@@ -0,0 +1,1 @@
+flask --app flask-server run --debug
--- flask-server/__init__.py
+++ flask-server/__init__.py
@@ -1,32 +1,60 @@
-import os
+from ariadne import QueryType, graphql_sync, make_executable_schema
+from ariadne.explorer import ExplorerGraphiQL
+from flask import Flask, jsonify, request
 
-from flask import Flask
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+
+query = QueryType()
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+@query.field("hello")
+def resolve_hello(_, info):
+    request = info.context
+    user_agent = request.headers.get("User-Agent", "Guest")
+    return "Hello, %s!" % user_agent
+
+
+schema = make_executable_schema(type_defs, query)
+
+app = Flask(__name__)
+
+# Retrieve HTML for the GraphiQL.
+# If explorer implements logic dependant on current request,
+# change the html(None) call to the html(request)
+# and move this line to the graphql_explorer function.
+explorer_html = ExplorerGraphiQL().html(None)
+
+
+@app.route("/graphql", methods=["GET"])
+def graphql_explorer():
+    # On GET request serve the GraphQL explorer.
+    # You don't have to provide the explorer if you don't want to
+    # but keep on mind this will not prohibit clients from
+    # exploring your API using desktop GraphQL explorer app.
+    return explorer_html, 200
+
+
+@app.route("/graphql", methods=["POST"])
+def graphql_server():
+    # GraphQL queries are always sent as POST
+    data = request.get_json()
+
+    # Note: Passing the request to the context is optional.
+    # In Flask, the current request is always accessible as flask.request
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value={"request": request},
+        debug=app.debug
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+if __name__ == "__main__":
+    app.run(debug=True)
