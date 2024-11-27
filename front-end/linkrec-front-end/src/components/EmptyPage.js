8aedf363d5f17dac81695c87933e875998751758
Dongho Chun <dongho.chun@student.uhasselt.be>
2024-11-27 22:47:41 +0100

    login functionality (sessions)

---
+++ front-end/linkrec-front-end/src/components/EmptyPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmptyPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '20%' }}>
+            <h1>Empty Page</h1>
+            <p>You are probably not logged in!</p>
+        </div>
+    );
+};
+
+export default EmptyPage;
