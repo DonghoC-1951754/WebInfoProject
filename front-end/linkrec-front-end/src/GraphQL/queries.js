a15ad9a15ce72a3bf6def7c2379ef3591b9de9e0
Dongho Chun <dongho.chun@student.uhasselt.be>
2024-11-30 11:13:58 +0100

    added http request

--- front-end/linkrec-front-end/src/GraphQL/queries.js
+++ front-end/linkrec-front-end/src/GraphQL/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 // Fetch all Users
 export const GET_USERS = gql`
@@ -8,7 +8,7 @@ export const GET_USERS = gql`
       firstName
       name
       email
-      location{
+      location {
         country
         city
         cityCode
@@ -23,6 +23,28 @@ export const CHECK_EMAIL_EXISTS = gql`
   query CheckEmailExists($email: String!) {
     userByEmail(email: $email) {
       id
+    }
+  }
+`;
+
+export const GET_ACTIVE_VACANCIES = gql`
+  query GetActiveVancancies($currentDate: String!) {
+    activeVancancies(currentDate: $currentDate) {
+      id
+      jobTitle
+      company{
+        name
+        location{
+          country
+          city
+          cityCode
+          street
+          houseNumber
+        }
+      }
+      requiredSkills
+      startDate
+      endDate
     }
   }
 `;
